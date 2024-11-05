b054930052535b9ce25be379fcece1823519d40e
eduZamboni <128424472+eduZamboni@users.noreply.github.com>
2024-11-05 00:02:14 -0300

    Primeiro commit

---
+++ src/views/Signin.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignIn: React.FC = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <p>Faça login na sua conta.</p>
+    </div>
+  );
+};
+
+export default SignIn;
